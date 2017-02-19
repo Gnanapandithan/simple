f41b4238a2c32a691e50f5da05d250b1707329a3
Gnanapandithan <m.gnanapandithan@gmail.com>
2017-02-19 00:08:08 -0600

    Update SimpleApplicationTests.java
    Test commit

--- simple/src/test/java/com/example/SimpleApplicationTests.java
+++ simple/src/test/java/com/example/SimpleApplicationTests.java
@@ -9,8 +9,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SimpleApplicationTests {
 
-	@Test
+	//@Test
 	public void contextLoads() {
+		while(true) {
+			System.out.println("welcome....");		
+		}
 	}
 
 }
