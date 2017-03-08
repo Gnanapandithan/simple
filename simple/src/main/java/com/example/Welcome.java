169cf69b99a2cee3c2dae3d7a4396aff9cf61b3a
371996 <Gnanapandithan.murugesan@cognizant.com>
2017-03-07 22:47:56 -0600

    test check-in

--- simple/src/main/java/com/example/Welcome.java
+++ simple/src/main/java/com/example/Welcome.java
@@ -3,6 +3,6 @@ package com.example;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println(" welcome to new branch check-in.....");
+		System.out.println(" welcome to new branch check-in.....##########");
 	}
 }
