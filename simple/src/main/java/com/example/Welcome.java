08069d49b1c6c7f276fef9dfa78844080f35f7e3
371996 <Gnanapandithan.murugesan@cognizant.com>
2017-03-07 23:42:55 -0600

    success committ

--- simple/src/main/java/com/example/Welcome.java
+++ simple/src/main/java/com/example/Welcome.java
@@ -3,7 +3,6 @@ package com.example;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println(" welcome to new branch check-in.....****");
-		System.out.println("111111111");
+		System.out.println(" welcome to new branch check-in.....success");
 	}
 }
