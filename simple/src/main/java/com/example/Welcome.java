270cf57b791e11dd97c9a9821fa63d595b66c319
371996 <Gnanapandithan.murugesan@cognizant.com>
2017-03-07 23:45:13 -0600

    Master commit

--- simple/src/main/java/com/example/Welcome.java
+++ simple/src/main/java/com/example/Welcome.java
@@ -4,6 +4,6 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println(" welcome to new branch check-in.....****");
-		System.out.println("111111111");
+		System.out.println("Master **************");
 	}
 }
